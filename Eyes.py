e57db3ca94cecfed8757bca779d49c0182391e9a
Kumar Anurag <kmranrg@gmail.com>
2018-07-27 21:58:40 +0530

    modified variables

--- Eyes.py
+++ Eyes.py
@@ -1,17 +1,17 @@
 import cv2
 
-faceCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 video_capture = cv2.VideoCapture(0)
 while True:
 	ret,frame = video_capture.read()
 
 	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-	faces = faceCascade.detectMultiScale(gray,
+	eyes = eyeCascade.detectMultiScale(gray,
 		scaleFactor=1.1,
 		minNeighbors=5,
 		minSize=(30,30))
 
-	for(x,y,w,h) in faces:
+	for(x,y,w,h) in eyes:
 		cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255),2)
 
 	cv2.imshow("Video",frame)
